1e26189fc80b9bdd5fba1c6444b5a15ad4da11cb
Lucas Sweany <39360190+sweany@users.noreply.github.com>
2022-04-05 17:54:03 +0000

    add counts

--- ipcollapse.py
+++ ipcollapse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 import ipaddress
 import argparse
 
@@ -14,7 +15,8 @@ def main():
     # positional arguments
     #parser.add_argument('mode', action='store', choices=['init', 'action', 'show'], help='operating mode')
     parser.add_argument('-f', action='store', nargs='?', default='', help='input file name')
-    parser.add_argument('-c', nargs='?', default='/24', help='smallest cidr subnet mask to apply (like /24)')
+    parser.add_argument('-m', nargs='?', default='/24', help='smallest cidr subnet mask to apply (like /24)')
+    parser.add_argument('-c', action='store_true', default=False, help='count IPs in each subnet')
 
     args = parser.parse_args()
     #print(type(args.profile))
@@ -26,7 +28,7 @@ def main():
         print("error opening input file: {}".format(e))
         sys.exit(1)
 
-    mask = args.c
+    mask = args.m
 
     iplist = []
 
@@ -38,8 +40,29 @@ def main():
     nets = [ipaddress.ip_network(_ip, strict=False) for _ip in iplist]
     cidrs = ipaddress.collapse_addresses(nets)
 
+    counts = {}
+    if args.c:
+        print("counting per cidr")
+        for cidr in cidrs:
+            for ip in iplist:
+                ip = re.sub("\/[0-9]*", "", ip)
+                #print("is {} within {} ?".format(ip, cidr))
+                if ipaddress.ip_address(ip) in cidr:
+                    if cidr in counts:
+                        counts[cidr] += 1
+                    else:
+                        counts[cidr] = 1
+
     for cidr in cidrs:
-        print(cidr)
+        if args.c:
+            print("counting per cidr")
+            try:
+                print(cidr)
+            except Exception as e:
+                print(e)
+            #print("{}: {}".format(cidr, counts[cidr]))
+        else:
+            print(cidr)
 
 
 
